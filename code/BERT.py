65295266b08c33f65b3cabb91874c92ccbfbc4f3
lMollidl <jialiang_zhou@brown.edu>
2022-04-24 21:47:06 -0400

    feat: bert accuracy 0.79

--- code/BERT.py
+++ code/BERT.py
@@ -1,8 +1,104 @@
+from lib2to3.pgen2 import token
 from preprocess import *
+import tensorflow as tf
+import transformers
+
+#######################
+# Some useful variables
+MAX_SEQUENCE_LENGTH = 512
+MODEL_NAME = 'distilbert-base-uncased'
+EMBEDDING_SIZE = 768
+LEARNING_RATE = 0.01
+BATCH_SIZE = 16
+#######################
+
+
 
 if __name__ == '__main__':
-
     # Preprocessing
     X_train, y_train, X_test, y_test = get_data()
+
+    # Get tokenizer
+    tokenizer = transformers.DistilBertTokenizerFast.from_pretrained(MODEL_NAME)
+    
+    # Tokenizing training and testing input and putting all inputs and labels 
+    # into proper format
+    tokenized_train_input = tokenizer(
+        X_train.values.tolist(),
+        truncation=True,
+        padding='max_length',
+        max_length=MAX_SEQUENCE_LENGTH,
+        return_tensors="tf"
+    )
+
+    tokenized_test_input = tokenizer(
+        X_test.values.tolist(),
+        truncation=True,
+        padding='max_length',
+        max_length=MAX_SEQUENCE_LENGTH,
+        return_tensors="tf"
+    )
+
+    train_input = dict(tokenized_train_input)
+    train_label = tf.convert_to_tensor(y_train)
+    test_input = dict(tokenized_test_input)
+    test_label = tf.convert_to_tensor(y_test)
+
+
+    # Get pretrained BERT weights
+    bert = transformers.TFDistilBertModel.from_pretrained(
+      MODEL_NAME,
+      num_labels=2
+      )
+    
+    # We are using feature based approach, so we are not going to 
+    # further train the BERT model
+    for layer in bert.layers:
+      layer.trainable = False
+    
+    # Initializing input, attention mask for BERT
+    input_ids = tf.keras.layers.Input(shape=(MAX_SEQUENCE_LENGTH,), dtype=tf.int32, name='input_ids')
+    attention_mask = tf.keras.layers.Input((MAX_SEQUENCE_LENGTH,), dtype=tf.int32, name='attention_mask')
+    
+    # Getting the output from bert
+    output = bert([input_ids, attention_mask]).last_hidden_state[:, 0, :]
+    
+    # Classification Layers
+    classification_layers = tf.keras.Sequential(
+      [
+        tf.keras.layers.Dense(
+          units=EMBEDDING_SIZE,
+          activation='relu',
+          name="dense_01",),
+        tf.keras.layers.Dense(
+          units=EMBEDDING_SIZE,
+          activation='relu',
+          name="dense_02",),
+        tf.keras.layers.Dense(
+          units=2,
+          activation='softmax',
+          name="softmax")
+      ])
+
+    # Feeding the output of BERT into classification layers 
+    output = classification_layers(output)
+
+    # Compiling the model and specifying loss, optimizer
+    model = tf.keras.models.Model(inputs=[input_ids, attention_mask], outputs=output)
+    model.compile(
+      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+      optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
+      metrics=['accuracy'])
+
+    # Training the model
+    model.fit(train_input,train_label,batch_size=BATCH_SIZE)
+
+    # Testing the model
+    test_loss, test_acc = model.evaluate(test_input,test_label)
+
+    print('\nTest accuracy: {}'.format(test_acc))
+    
+    
+    
     
     # If you need tokenization, see how I did it in LSTM.py!
--- code/LSTM.py
+++ code/LSTM.py
@@ -1,6 +1,10 @@
 from preprocess import *
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
+from gym import make
+import numpy as np
+import tensorflow as tf
+
 
 if __name__ == '__main__':
 
@@ -21,5 +25,85 @@ if __name__ == '__main__':
     test_token = tokenizer.texts_to_sequences(X_test.values)
     test_token = pad_sequences(test_token)
     # print(tokenizer.sequences_to_texts([X_token[0]]))
+
+
+### I need to modify the follwing code 
+
+class LSTM_Seq2Dis(tf.keras.Model):
+	def __init__(self, window_size, vocab_size):
+		###### DO NOT CHANGE ##############
+		super(LSTM_Seq2Dis, self).__init__()
+		self.vocab_size = vocab_size 
+		self.french_window_size = window_size 
+		######^^^ DO NOT CHANGE ^^^##################
+		def make_vars(*dim,initializer=tf.random.normal):
+			return tf.Variable(initializer(dim,stddev=0.1))
+
+		# TODO:
+		# 1) Define any hyperparameters
+
+		# Define batch size and optimizer/learning rate
+		self.batch_size = 50 
+		self.embedding_size = 50 
+		self.learning_rate=5e-3
+		self.RNN=self.embedding_size
+		self.dropRate=0.5
+
+		# 2) Define embeddings, encoder, decoder, and feed forward layers
+		self.embeddingVocab=make_vars(self.vocab_size, self.embedding_size)
+		self.lstmdis=tf.keras.layers.LSTM(self.RNN,return_sequences=True,return_state=True,recurrent_dropout=0.3)
+		self.drop=tf.keras.layers.Dropout(self.dropRate)
+		# first type of dense
+		self.denseTypeOne=tf.keras.layers.Dense(self.vocab_size,activation='relu')
+		# second type of dense
+		self.denseTypeTwo=tf.keras.layers.Dense(self.vocab_size,activation='sigmoid')
+		# max pooling
+		self.maxpool=tf.keras.layers.MaxPool1D()
+		self.optimizer=tf.keras.optimizers.Adam(self.learning_rate)
+
+	@tf.function
+	def call(self, batched_input):
+
+		embededSentence=tf.nn.embedding_lookup(self.embeddingVocab,batched_input)
+		sentence_output, final_memory_state,final_carry_state=self.lstmdis(embededSentence,None)
+		poolingRe=self.maxpool(final_memory_state)
+		denseOne=self.denseTypeOne(poolingRe)
+		dropOne=self.drop(denseOne,training=True)
+		denseTwo=self.denseTypeOne(dropOne)
+		dropTwo=self.drop(denseTwo,training=True)
+		prbs=self.denseTypeTwo(dropTwo)		
+		return prbs
+
+	def accuracy_function(self, prbs, labels, mask):
+		"""
+		DO NOT CHANGE
+		Computes the batch accuracy
+
+		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
+		:param labels:  integer tensor, word prediction labels [batch_size x window_size]
+		:param mask:  tensor that acts as a padding mask [batch_size x window_size]
+		:return: scalar tensor of accuracy of the batch between 0 and 1
+		"""
+
+		decoded_symbols = tf.argmax(input=prbs, axis=2)
+		accuracy = tf.reduce_mean(tf.boolean_mask(tf.cast(tf.equal(decoded_symbols, labels), dtype=tf.float32),mask))
+		return accuracy
+
+
+	def loss_function(self, prbs, labels, mask):
+		"""
+		Calculates the total model cross-entropy loss after one forward pass.
+		Please use reduce sum here instead of reduce mean to make things easier in calculating per symbol accuracy.
+
+		:param prbs:  float tensor, word prediction probabilities [batch_size x window_size x english_vocab_size]
+		:param labels:  integer tensor, word prediction labels [batch_size x window_size]
+		:param mask:  tensor that acts as a padding mask [batch_size x window_size]
+		:return: the loss of the model as a tensor
+		"""
+		lossTensor=tf.keras.losses.sparse_categorical_crossentropy(labels,prbs)*mask
+		loss=tf.math.reduce_sum(lossTensor) 	
+
+
+		return loss
 
     
